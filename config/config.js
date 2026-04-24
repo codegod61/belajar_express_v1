e524da9c01fc1f7d79f2e33cfd14bae3110fdd55
M. TAUFIQ HIDAYAT <mtaufiq.2021@student.uny.ac.id>
2026-04-24 14:54:42 +0800

    memperbarui config.js

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ export default {
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME_TEST,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
